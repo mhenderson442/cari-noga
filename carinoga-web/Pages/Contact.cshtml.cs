6bf01cda5776c72ffba3a6d144325a59c0e61e89
Michael Henderson <mike@mohenderson.com>
2023-05-05 18:43:46 -0400

    Stubbed out pages.

---
+++ carinoga-web/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CariNoga.Web.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
