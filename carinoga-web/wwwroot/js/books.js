cb1f7d1a2134e636f92643b9b61f5d1db61f9991
Michael Henderson <mike@hendersonconsulting.com>
2023-05-20 10:09:23 -0400

    Added some vanilla JS.

--- carinoga-web/wwwroot/js/books.js
+++ carinoga-web/wwwroot/js/books.js
@@ -1,1 +1,23 @@
-﻿alert("I agree to be awesome today");
+﻿let books = document.getElementsByClassName("books");
+
+Array.from(books).forEach(bookNavigationClickEvent);
+
+function bookNavigationClickEvent(element) {
+    document.getElementById(element.id).addEventListener(
+        "click",
+        () => {
+            hideAllBooksArticles();
+            showSelectedArticle(element.id);;
+        }
+    );
+}
+function hideAllBooksArticles() {
+    Array.from(books).forEach((book) => {
+        document.getElementById(`${book.id}-article`).hidden = true;
+    });
+}
+
+function showSelectedArticle(id) {
+    let bookId = `${id}-article`;
+    document.getElementById(bookId).hidden = false;
+};
